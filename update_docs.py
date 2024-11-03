5bedbb47ac6bcac1521814fe95726cc13d93efa3
nisargbhatt11 <89977747+nisargbhatt11@users.noreply.github.com>
2024-11-04 04:36:49 +0530

    Create update_docs.py

---
+++ update_docs.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+# Get the current date and time
+current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+# Open the file in append mode and write the update
+with open('docs/index.html', 'a') as file:
+    file.write(f'<p>Updated automatically by Python script on {current_time}!</p>\n')
+
+print("Documentation updated with date and time!")
