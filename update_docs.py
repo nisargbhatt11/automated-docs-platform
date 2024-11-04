9b45cc4fa832d7fef1a6ecc4124c474c40a28708
nisargbhatt11 <89977747+nisargbhatt11@users.noreply.github.com>
2024-11-04 22:19:01 +0530

    try and catch block

--- update_docs.py
+++ update_docs.py
@@ -3,9 +3,9 @@ from datetime import datetime
 # Get the current date and time
 current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-# Open the file in append mode and write the update
-with open('docs/index.html', 'a') as file:
-    file.write(f'<p>Updated automatically by Python script on {current_time}!</p>\n')
-
-print("Documentation updated with date and time: " + current_time)
-#test commit 21:01 EU
+try:
+    with open('docs/index.html', 'a') as file:
+        file.write(f'<p>Updated automatically by Python script on {current_time}!</p>\n')
+    print("Update successful.")
+except Exception as e:
+    print("An error occurred:", e)
