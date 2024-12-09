ee61fc950349b3235de8fc5b46eb6c2838531f86
nisargbhatt11 <89977747+nisargbhatt11@users.noreply.github.com>
2024-12-10 01:25:14 +0530

    Update update_docs.py
    Now making a log txt file to check the changes made by the pipeline

--- update_docs.py
+++ update_docs.py
@@ -3,9 +3,13 @@ from datetime import datetime
 # Get the current date and time
 current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
+# Specify the text file name
+file_path = 'docs/update_log.txt'  # You can replace this with any path you like
+
 try:
-    with open('docs/index.html', 'a') as file:
-        file.write(f'<p>Updated automatically by Python script on {current_time}!</p>\n')
+    # Open the text file in append mode and write the update message
+    with open(file_path, 'a') as file:
+        file.write(f'Updated automatically by Python script on {current_time}!\n')
     print("Update successful.")
 except Exception as e:
     print("An error occurred:", e)
